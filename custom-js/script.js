c23357b091fa70b6017191620ae1c470661e132b
SarabanTahura1 <bristiakash01796@gmail.com>
2023-03-04 23:31:18 +0600

    fixing functionality

--- custom-js/script.js
+++ custom-js/script.js
@@ -7,54 +7,17 @@ const loadApps = async () => {
 
 const showMoreButton = document.getElementById("see-more-btn");
 const sortByDateButton = document.querySelector('#sort-by-date');
-let isSorted = false;
+let showMoreClicked = false;
 
 
 const displayApps = apps => {
-  console.log(apps);
   const appContainer = document.getElementById('app-container');
-  Apps = apps.slice(0, 6);
-  Apps.forEach(app => {
-    const appDiv = document.createElement('div');
-    appDiv.classList.add('col');
-    appDiv.innerHTML = `
-    <div class="card h-100">
-    <img src="${app.image}" class="card-img-top" alt="...">
-    <div class="card-body">
-   <h2>Features</h2>
-      
-   <ol>
-   <li>${app.features[0]}</li>
-   <li>${app.features[1]}</li>
-   <li>${app.features[2]}</li>
-
-</ol>
-   
-      <hr>
-      <div class ="d-flex" >
-      <div class ="me-5" >
-      <h5 class="card-title">${app.name}</h5>
-      <p class="card-text">${app.published_in}</p>
-      </div>
-      <div>
-      <button onclick="displayAppDetails()" id ="modal-btn" type="button" class="btn btn-outline-success btn-floating ms-5 " data-mdb-ripple-color="dark" data-bs-toggle="modal" data-bs-target="#appDetailModal">
-            Details
-          </button>
-       
-
-      </div>
-    </div>
-  </div>
-  `;
-    appContainer.appendChild(appDiv);
 
 
-  })
-
-
+  //All Data Show
   let remainingCards = apps.slice(6);
-
   showMoreButton.addEventListener("click", () => {
+    showMoreClicked = true;
     spinner.style.display = "block";
     showMoreButton.style.display = "none";
     setTimeout(() => {
@@ -97,52 +60,88 @@ const displayApps = apps => {
   });
 
   sortByDateButton.addEventListener('click', () => {
-
-    function compareDates(a, b) {
-      const dateA = new Date(a.published_in);
-      const dateB = new Date(b.published_in);
-      return dateA - dateB;
-    }
-    apps.sort(compareDates);
-    appContainer.innerHTML = '';
-    console.log(apps);
-    Apps = apps.slice(0, 6);
-    Apps.forEach(app => {
-      const appDiv = document.createElement('div');
-      appDiv.classList.add('col');
-      appDiv.innerHTML = `
-      <div class="card h-100">
-      <img src="${app.image}" class="card-img-top" alt="...">
-      <div class="card-body">
-     <h2>Features</h2>
-        
-        <ol>
-        <li>${app.features[0]}</li>
-        <li>${app.features[1]}</li>
-        <li>${app.features[2]}</li>
-        </ol>
-     
-        <hr>
-        <div class ="d-flex" >
-        <div class ="me-5" >
-        <h5 class="card-title">${app.name}</h5>
-        <p class="card-text">${app.published_in}</p>
-        </div>
-        <div>
-        <button onclick="displayAppDetails()" id ="modal-btn" type="button" class="btn btn-outline-success btn-floating ms-5 " data-mdb-ripple-color="dark" data-bs-toggle="modal" data-bs-target="#appDetailModal">
-              Details
-            </button>
+    if (showMoreClicked === true) {
+      function compareDates(a, b) {
+        const dateA = new Date(a.published_in);
+        const dateB = new Date(b.published_in);
+        return dateA - dateB;
+      }
+      apps.sort(compareDates);
+      appContainer.innerHTML = '';
+      apps.forEach(app => {
+        const appDiv = document.createElement('div');
+        appDiv.classList.add('col');
+        appDiv.innerHTML = `
+        <div class="card h-100">
+        <img src="${app.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+       <h2>Features</h2>
+          
+          <ol>
+          <li>${app.features[0]}</li>
+          <li>${app.features[1]}</li>
+          <li>${app.features[2]}</li>
+          </ol>
+       
+          <hr>
+          <div class ="d-flex" >
+          <div class ="me-5" >
+          <h5 class="card-title">${app.name}</h5>
+          <p class="card-text">${app.published_in}</p>
+          </div>
+          <div>
+          <button onclick="displayAppDetails()" id ="modal-btn" type="button" class="btn btn-outline-success btn-floating ms-5 " data-mdb-ripple-color="dark" data-bs-toggle="modal" data-bs-target="#appDetailModal">
+                Details
+              </button>
+          </div>
         </div>
       </div>
-    </div>
-    `;
-      appContainer.appendChild(appDiv);
+      `;
+        appContainer.appendChild(appDiv);
 
 
-    })
+      })
+    } else {
+      function compareDates(a, b) {
+        const dateA = new Date(a.published_in);
+        const dateB = new Date(b.published_in);
+        return dateA - dateB;
+      }
+      sixCards.sort(compareDates);
+      appContainer.innerHTML = '';
+      sixCards.forEach(app => {
+        const appDiv = document.createElement('div');
+        appDiv.classList.add('col');
+        appDiv.innerHTML = `
+        <div class="card h-100">
+        <img src="${app.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+       <h2>Features</h2>
+          
+          <ol>
+          <li>${app.features[0]}</li>
+          <li>${app.features[1]}</li>
+          <li>${app.features[2]}</li>
+          </ol>
+       
+          <hr>
+          <div class ="d-flex" >
+          <div class ="me-5" >
+          <h5 class="card-title">${app.name}</h5>
+          <p class="card-text">${app.published_in}</p>
+          </div>
+          <div>
+          <button onclick="displayAppDetails()" id ="modal-btn" type="button" class="btn btn-outline-success btn-floating ms-5 " data-mdb-ripple-color="dark" data-bs-toggle="modal" data-bs-target="#appDetailModal">
+                Details
+              </button>
+          </div>
+        </div>
+      </div>
+      `;
+        appContainer.appendChild(appDiv);
+      })
+    }
   });
-
-
 }
 
 
