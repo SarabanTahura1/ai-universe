0368ac73a6505e488bf1eed3ce571cf667942761
SarabanTahura1 <bristiakash01796@gmail.com>
2023-03-04 18:48:23 +0600

    see more btn functionality added

--- custom-js/script.js
+++ custom-js/script.js
@@ -1,14 +1,16 @@
-const loadApps= async()=>{
-const url = `https://openapi.programming-hero.com/api/ai/tools`
-const res = await fetch(url);
-const data = await res.json();
-displayApps(data.data.tools);
+const loadApps = async () => {
+  const url = `https://openapi.programming-hero.com/api/ai/tools`
+  const res = await fetch(url);
+  const data = await res.json();
+  displayApps(data.data.tools);
 }
-const displayApps = apps => {
-   // console.log(apps);
 
-   const appContainer = document.getElementById('app-container');
-   apps.forEach(app =>{
+const showMoreButton = document.getElementById("see-more-btn");
+const displayApps = apps => {
+  // console.log(apps);
+  const appContainer = document.getElementById('app-container');
+  Apps = apps.slice(0, 6);
+  Apps.forEach(app => {
     const appDiv = document.createElement('div');
     appDiv.classList.add('col');
     appDiv.innerHTML = `
@@ -28,17 +30,78 @@ const displayApps = apps => {
       <p class="card-text">${app.published_in}</p>
       </div>
       <div>
-      <button type="button" class="btn btn-outline-success btn-floating ms-5 " data-mdb-ripple-color="dark">
+      <button id ="modal-btn" type="button" class="btn btn-outline-success btn-floating ms-5 " data-mdb-ripple-color="dark" data-bs-toggle="modal" data-bs-target="#appDetailModal">
             →
           </button>
+       
+
       </div>
     </div>
   </div>
   `;
-  appContainer.appendChild(appDiv);
+    appContainer.appendChild(appDiv);
 
-   })
+    // document.getElementById('see-more-btn').addEventListener('click', function () {
+    //   //showing spinner
+    //   //toggleSpinner(true);
+    //   delete json(slice);
+    // })
+  })
+
+
+  let remainingCards = apps.slice(6);
+
+  showMoreButton.addEventListener("click", () => {
+    spinner.style.display = "block";
+    showMoreButton.style.display = "none";
+    setTimeout(() => {
+      for (let i = 0; i < remainingCards.length; i++) {
+        const appDiv = document.createElement('div');
+        appDiv.classList.add('col');
+        appDiv.innerHTML = `
+      <div class="card h-100">
+      <img src="${remainingCards[i].image}" class="card-img-top" alt="...">
+      <div class="card-body">
+     <h2>Features</h2>
+        
+        <ol>
+          <li>${remainingCards[i].features}</li>
+       </ol>
+     
+        <hr>
+        <div class ="d-flex" >
+        <div class ="me-5" >
+        <h5 class="card-title">${remainingCards[i].name}</h5>
+        <p class="card-text">${remainingCards[i].published_in}</p>
+        </div>
+        <div>
+        <button id ="modal-btn" type="button" class="btn btn-outline-success btn-floating ms-5 " data-mdb-ripple-color="dark" data-bs-toggle="modal" data-bs-target="#appDetailModal">
+              →
+            </button>
+         
+  
+        </div>
+      </div>
+    </div>
+    `;
+        appContainer.appendChild(appDiv);
+      }
+      spinner.style.display = "none";
+    }, 2000);
+  });
+
 }
+//display app
+
+
+
+const displayAppDetails = app => {
+
+  const modalTitle = document.getElementById('appDetailModalLabel');
+  modalTitle.innerText = app.name;
+}
+
+
 
 
 
